086b32f8175d701b40fe52965f52eb71e7ca9b48
vito <suraci.alex@gmail.com>
2024-07-12 21:21:43 -0400

    build
    Signed-off-by: vito <suraci.alex@gmail.com>

--- build/swirly.js
+++ build/swirly.js
@@ -2,10 +2,10 @@ function GoroutineGroup(location) {
   this.location = location;
   this.goroutines = [];
   this.groups = {};
-};
+}
 
-var packagePathRegex = /.*src\//
-GoroutineGroup.prototype.packagePath = function() {
+var packagePathRegex = /.*src\//;
+GoroutineGroup.prototype.packagePath = function () {
   return this.location.path.replace(packagePathRegex, "");
 };
 
@@ -15,22 +15,24 @@ function GoroutineStack(id, state, waiting, isLocked) {
   this.waiting = waiting;
   this.isLocked = isLocked;
   this.stack = "";
-};
+}
 
-GoroutineStack.prototype.pushStackLine = function(line) {
+GoroutineStack.prototype.pushStackLine = function (line) {
   this.stack += line + "\n";
 };
 
-GoroutineStack.prototype.matchesFilter = function(filter) {
-  return this.stack.indexOf(filter) != -1 ||
+GoroutineStack.prototype.matchesFilter = function (filter) {
+  return (
+    this.stack.indexOf(filter) != -1 ||
     this.state.indexOf(filter) != -1 ||
-    (this.waiting && this.waiting.indexOf(filter) != -1);
+    (this.waiting && this.waiting.indexOf(filter) != -1)
+  );
 };
 
 var callArgRegex = /\(((0x[a-f0-9]+|\.\.\.)(, )?)+\)/;
 var locationRegex = /\s+([^\s]+)( .*)?$/;
 
-GoroutineStack.prototype.registerIn = function(group) {
+GoroutineStack.prototype.registerIn = function (group) {
   var stackLines = this.stack.split("\n");
   if (stackLines[stackLines.length - 1] == "") {
     stackLines.pop();
@@ -43,14 +45,14 @@ GoroutineStack.prototype.registerIn = function(group) {
 
   for (var i = stackLines.length - 2; i >= 0; i -= 2) {
     var call = stackLines[i];
-    var path = stackLines[i+1];
+    var path = stackLines[i + 1];
 
     var pathMatch = path.match(locationRegex);
 
     var location = {
       call: call.replace(callArgRegex, "()"),
-      path: pathMatch[1]
-    }
+      path: pathMatch[1],
+    };
 
     if (!group.groups[location.path]) {
       group.groups[location.path] = new GoroutineGroup(location);
@@ -62,7 +64,7 @@ GoroutineStack.prototype.registerIn = function(group) {
   group.goroutines.push(this);
 };
 
-GoroutineStack.prototype.waitInSeconds = function() {
+GoroutineStack.prototype.waitInSeconds = function () {
   if (!this.waiting) {
     return 0;
   }
@@ -71,68 +73,88 @@ GoroutineStack.prototype.waitInSeconds = function() {
   var n = parseInt(s[0], 10);
 
   switch (s[1]) {
-  case "hours":
-    return n * 60 * 60;
-  case "minutes": // only unit actually reported
-    return n * 60;
-  case "seconds":
-    return n;
-  default:
-    console.log("unknown unit:", s[1]);
-    return 0;
+    case "hours":
+      return n * 60 * 60;
+    case "minutes": // only unit actually reported
+      return n * 60;
+    case "seconds":
+      return n;
+    default:
+      console.log("unknown unit:", s[1]);
+      return 0;
   }
 };
 
-var Goroutine = React.createClass({displayName: "Goroutine",
-  render: function() {
+var Goroutine = React.createClass({
+  displayName: "Goroutine",
+  render: function () {
     var classes = ["goroutine"];
     if (this.props.data.waiting) {
       classes.push("waiting");
     }
 
-    return (
-        React.createElement("div", {className: classes.join(" ")}, 
-          React.createElement("div", {className: "id"}, "#", this.props.data.id), 
-          React.createElement("div", {className: "status"}, this.props.data.state), 
-          React.createElement("div", {className: "waiting"}, this.props.data.waiting), 
-          React.createElement("pre", {className: "stack-trace"}, this.props.data.stack)
-        )
+    return React.createElement(
+      "div",
+      { className: classes.join(" ") },
+      React.createElement("div", { className: "id" }, "#", this.props.data.id),
+      React.createElement(
+        "div",
+        { className: "status" },
+        this.props.data.state,
+      ),
+      React.createElement(
+        "div",
+        { className: "waiting" },
+        this.props.data.waiting,
+      ),
+      React.createElement(
+        "pre",
+        { className: "stack-trace" },
+        this.props.data.stack,
+      ),
     );
-  }
+  },
 });
 
 var boringRegex = /src\/([a-z]+)\//;
-var StackGroup = React.createClass({displayName: "StackGroup",
-  getInitialState: function() {
-    return {expanded: true};
+var StackGroup = React.createClass({
+  displayName: "StackGroup",
+  getInitialState: function () {
+    return { expanded: true };
   },
 
-  handleToggle: function() {
-    this.setState({expanded: !this.state.expanded});
+  handleToggle: function () {
+    this.setState({ expanded: !this.state.expanded });
   },
 
-  render: function() {
+  render: function () {
     var subGroups = [];
     var goroutines = [];
 
     if (this.state.expanded) {
       for (var i in this.props.data.groups) {
         var group = this.props.data.groups[i];
-        subGroups.push(React.createElement(StackGroup, {key: group.location.path, data: group}));
+        subGroups.push(
+          React.createElement(StackGroup, {
+            key: group.location.path,
+            data: group,
+          }),
+        );
       }
 
       for (var i in this.props.data.goroutines) {
         var goroutine = this.props.data.goroutines[i];
-        goroutines.push(React.createElement(Goroutine, {key: goroutine.id, data: goroutine}));
+        goroutines.push(
+          React.createElement(Goroutine, {
+            key: goroutine.id,
+            data: goroutine,
+          }),
+        );
       }
     }
 
     if (this.props.data.location.path === undefined) {
-      return (
-          React.createElement("div", {className: "root-group"}, 
-            subGroups
-          )
-      );
+      return React.createElement("div", { className: "root-group" }, subGroups);
     }
 
     var classes = ["stack-group"];
@@ -140,51 +162,56 @@ var StackGroup = React.createClass({displayName: "StackGroup",
       classes.push("boring");
     }
 
-    return (
-        React.createElement("div", {className: classes.join(" ")}, 
-          React.createElement("div", {className: "title", onClick: this.handleToggle}, 
-            React.createElement("h2", null, this.props.data.packagePath()), 
-            React.createElement("h1", null, this.props.data.location.call)
-          ), 
-          React.createElement("div", {className: "stack-content"}, 
-            goroutines, 
-            subGroups
-          )
-        )
+    return React.createElement(
+      "div",
+      { className: classes.join(" ") },
+      React.createElement(
+        "div",
+        { className: "title", onClick: this.handleToggle },
+        React.createElement("h2", null, this.props.data.packagePath()),
+        React.createElement("h1", null, this.props.data.location.call),
+      ),
+      React.createElement(
+        "div",
+        { className: "stack-content" },
+        goroutines,
+        subGroups,
+      ),
     );
-  }
+  },
 });
 
-var Root = React.createClass({displayName: "Root",
-  getInitialState: function() {
+var Root = React.createClass({
+  displayName: "Root",
+  getInitialState: function () {
     return {
       filter: "",
       goroutines: [],
-    }
+    };
   },
 
-  handleFilter: function(event) {
-    this.setState({filter: event.target.value});
+  handleFilter: function (event) {
+    this.setState({ filter: event.target.value });
   },
 
-  handleFetch: function(event) {
+  handleFetch: function (event) {
     var root = this;
 
     var url = React.findDOMNode(this.refs.url).value;
 
     $.ajax({
       url: url,
-      success: function(response) {
-        root.setState({goroutines: parseGoroutines(response)});
-      }
+      success: function (response) {
+        root.setState({ goroutines: parseGoroutines(response) });
+      },
     });
 
     event.preventDefault();
   },
 
-  handleDump: function(event) {
+  handleDump: function (event) {
     var dump = React.findDOMNode(this.refs.dump).value;
-    this.setState({goroutines: parseGoroutines(dump)});
+    this.setState({ goroutines: parseGoroutines(dump) });
 
     // clear textarea; seems to kill performance if it keeps the data in there
     React.findDOMNode(this.refs.dump).value = "";
@@ -192,7 +219,7 @@ var Root = React.createClass({displayName: "Root",
     event.preventDefault();
   },
 
-  render: function() {
+  render: function () {
     var filteredGoroutines = [];
     for (var i in this.state.goroutines) {
       var goroutine = this.state.goroutines[i];
@@ -203,33 +230,57 @@ var Root = React.createClass({displayName: "Root",
 
     var rootGroup = goroutineGroups(filteredGoroutines);
 
-    return (
-        React.createElement("div", {className: "swirly"}, 
-          React.createElement("div", {className: "controls"}, 
-            React.createElement("div", {className: "filter"}, 
-              React.createElement("input", {type: "text", placeholder: "filter...", onChange: this.handleFilter})
-            ), 
+    return React.createElement(
+      "div",
+      { className: "swirly" },
+      React.createElement(
+        "div",
+        { className: "controls" },
+        React.createElement(
+          "div",
+          { className: "filter" },
+          React.createElement("input", {
+            type: "text",
+            placeholder: "filter...",
+            onChange: this.handleFilter,
+          }),
+        ),
 
-            React.createElement("form", {className: "fetch", onSubmit: this.handleFetch}, 
-              React.createElement("input", {type: "text", placeholder: "url", ref: "url"}), 
-              React.createElement("input", {type: "submit", value: "fetch"})
-            ), 
+        React.createElement(
+          "form",
+          { className: "fetch", onSubmit: this.handleFetch },
+          React.createElement("input", {
+            type: "text",
+            placeholder: "url",
+            ref: "url",
+          }),
+          React.createElement("input", { type: "submit", value: "fetch" }),
+        ),
 
-            React.createElement("form", {className: "dump", onSubmit: this.handleDump}, 
-              React.createElement("textarea", {rows: "1", type: "text", placeholder: "dump", ref: "dump"}), 
-              React.createElement("input", {type: "submit", value: "set"})
-            )
-          ), 
+        React.createElement(
+          "form",
+          { className: "dump", onSubmit: this.handleDump },
+          React.createElement("textarea", {
+            rows: "1",
+            type: "text",
+            placeholder: "dump",
+            ref: "dump",
+          }),
+          React.createElement("input", { type: "submit", value: "set" }),
+        ),
+      ),
 
-          React.createElement("div", {className: "goroutines"}, 
-            React.createElement(StackGroup, {data: rootGroup})
-          )
-        )
+      React.createElement(
+        "div",
+        { className: "goroutines" },
+        React.createElement(StackGroup, { data: rootGroup }),
+      ),
     );
-  }
+  },
 });
 
-var goroutineHeaderRegex = /^goroutine (\d+) \[([^,\]]+)(, ([^,\]]+))?(, locked to thread)?\]:$/
+var goroutineHeaderRegex =
+  /^goroutine (\d+) [^\[]*\[([^,\]]+)(, ([^,\]]+))?(, locked to thread)?\]:$/;
 function parseGoroutines(dump) {
   var dumpLines = dump.split("\n");
   var goroutines = [];
@@ -274,7 +325,4 @@ function goroutineGroups(goroutines) {
   return rootGroup;
 }
 
-React.render(
-  React.createElement(Root, null),
-  document.getElementById('dumps')
-);
+React.render(React.createElement(Root, null), document.getElementById("dumps"));
